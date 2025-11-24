75f32e8a74160bc90d42789f9ca644839f4de114
Swathi03-ai <nagaswathi.koka@gmail.com>
2025-11-24 10:58:25 -0600

    ENH/SAGE-23:Add DashboardPage component with DashboardChart

---
+++ frontend/src/pages/DashboardPage.js
@@ -0,0 +1,10 @@
+import DashboardChart from "../components/DashboardChart";
+
+export default function DashboardPage() {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <DashboardChart />
+    </div>
+  );
+}
